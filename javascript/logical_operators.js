abf37585cdd36f9f72e0bd01dc55ad72cc993110
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-17 11:23:45 +0300

    js: logical operators, OR

---
+++ javascript/logical_operators.js
@@ -0,0 +1,24 @@
+/**
+ * ^ Js Logical operators
+ * * namely: - OR ( || )
+ * *              - AND ( && )
+ * *              - NOT ( ! )
+ * *              - Nullish coalescing ( ?? )
+ * ~ Although they are called “logical”, 
+ * ~ they can be applied to values of any type, not only boolean. 
+ * ~ Their result can also be of any type.
+ * 
+*/
+
+//* Logical OR ( || )
+    // its denoted using two vertical line symbols
+    // result = a || b;
+    // console.log(result);  
+    
+    //^ there are four possible logical combinations:
+    // true || true is true
+    // false || true is true
+    // true || false is true
+    // false || false is false
+    console.log( false || true);   // Output: true
+
