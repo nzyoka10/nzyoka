9c2097c531761db9a01bb4bcbdfee6853442d0ed
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-01-30 12:38:35 +0300

    Js: functions introduction

---
+++ javascript/functions.js
@@ -0,0 +1,9 @@
+//^ Java script functions
+    //=============================
+    function sayHi()
+    {
+        console.log("Hello, Js functions!");
+    }
+
+    // calling the function
+    sayHi();
