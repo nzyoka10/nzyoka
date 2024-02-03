b54eb6c5bf1644bab119e05add450b3abcd9668a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-03 11:16:08 +0300

    function add7 :: to return num + 7!

---
+++ javascript/assignment/index.js
@@ -0,0 +1,14 @@
+//alert("Hello Eric!");
+//console.log(23 + 9);
+/**
+ ** write function called add7
+ *@add : function add, take number
+ ** return  :: number then ADD it to 7
+ *^ 
+ */
+function add7(num, sum) {
+    var  num = 3;
+    //alert(sum = num + 7);
+    console.log(num+7)
+}
+add7();
