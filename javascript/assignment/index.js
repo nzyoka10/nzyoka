59dbce212f5b24ddd779be910561e68b2c6b5a9a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-03 12:25:50 +0300

    Js: more of functions, JS is fun!

--- javascript/assignment/index.js
+++ javascript/assignment/index.js
@@ -6,9 +6,36 @@
  ** return  :: number then ADD it to 7
  *^ 
  */
-function add7(num, sum) {
-    var  num = 3;
-    //alert(sum = num + 7);
-    console.log(num+7)
-}
-add7();
+    function add7(num) 
+    {
+        var  num = 3;
+        //alert(sum = num + 7);
+        console.log(num+7)
+    }
+    add7();
+
+/**
+ ** Write a function called multiply that takes 2 numbers and returns their product.
+ * @param {myNum1} myNum1 : The first number
+ * @param {myNum2}  myNum2 : The second number
+ * @return {multiply} multiply :: The product of `x` and `y`.
+ * 
+ */
+// let a = 10, b = 2;
+    function multiply (myNum1, myNum2, sum) 
+    {
+        console.log("myNum1: " + myNum1);
+        console.log("myNum2: " + myNum2);
+        console.log("mutiply: " + myNum1 + " * " + myNum2);
+        console.log("My  answer is: " + (myNum1*myNum2));
+        
+        console.log("\n ");       
+    }
+    multiply (10, 5);
+
+/**
+ * function capitalize
+ * takes a string and returns  that string, with ONLY 
+ * * the first letter capitalized
+ * 
+ */
