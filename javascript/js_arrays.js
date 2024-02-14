48b482486409f38ce2ba2528534ef14820e31d61
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-14 14:57:34 +0300

    Js arrays

---
+++ javascript/js_arrays.js
@@ -0,0 +1,20 @@
+/**
+ * ! ======   Arrays in JavaScript ----
+ * create array below
+ */
+
+// emapty array
+    let array = [];
+    console.log(array);  
+
+// Arrays start at Zero(0)
+    const k = ["Apple", "Mango", "Android", "Sympian"]
+    console.log(k);  //~ Output: [ 'Apple', 'Mango', 'Android', 'Sympian' ]
+    console.log(`At index 0, ${k[0]} `); 
+    console.log(`At index 1, ${k[1]}`);
+    console.log(`At index 2, ${k[2]}`);
+    console.log(`At index 3, ${k[3]}`);
+
+
+
+//console.log(`The ${k[1]} is Mango, at index 1`);  // Output: Mango 
