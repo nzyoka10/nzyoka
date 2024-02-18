160d62cd624cb49cabca5ccdab326b6231fd3578
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-18 12:25:32 +0300

    js loops

---
+++ javascript/loops.js
@@ -0,0 +1,23 @@
+/**
+ * *   Loops execute a block of code a number of times
+ *^    ------     Instead of writing:   --------
+                    text += cars[0] + "<br>";
+                    text += cars[1] + "<br>";
+                    text += cars[2] + "<br>";
+                    text += cars[3] + "<br>";
+                    text += cars[4] + "<br>";
+                    text += cars[5] + "<br>";
+    ** We can use a loop to do this in one line of code:
+    *!             for(var i = 0; i < cars.length; i++)
+    *!             {
+    *!                  text += cars[i] + "<br>";
+    *!              }
+    *
+    * * ===== Types of Loops in JavaScript ------
+    * ^        1. For loop --> loop through a fixed amount of items (use when you know how many times you want the loop to run)
+    * !      There are two main types of loops in JavaScript:
+     * *         a) "for" loops, which iterate a fixed number of times.
+     * *         b) "while" loops, which continue until a condition is false.
+     * ^      2. 
+    * 
+ */
