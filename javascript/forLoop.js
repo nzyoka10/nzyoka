a695aa9181d5e1eff0cf82841ba9876305843b5e
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-18 12:37:15 +0300

    javascripts for loop

---
+++ javascript/forLoop.js
@@ -0,0 +1,20 @@
+// for loop statement creates a loop with 3 optional expressions
+// print numbers 1 to 10
+    // for (let k = 1; k <= 10; k++) 
+    // {
+        // console.log(k);
+        // prints the numbers 1 to 10
+    // }
+
+// Example 2:
+console.log("Array of the most fastest cars of the world!");
+    const fastestCars = [
+        "Bugatti Chiron Super Sport 300+",
+        "Koenigsegg Jesko Absolut",
+        "SSC Tuatara",
+        "Hennessey Venom F5",
+        "Rimac C_Two"
+    ];
+  
+  console.log(fastestCars);
+  
