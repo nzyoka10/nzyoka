c9d412ceda5a7edfbc2535a3de07fed054775c3a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-18 18:10:24 +0300

    while loop in javascript

---
+++ javascript/whileLoop.js
@@ -0,0 +1,32 @@
+/**
+ * *    Javascript while loop
+ * ~       --> Loops can execute a block of code as long as a specified condition is true.
+ * 
+ * ~       SYNTAX
+ * &            while (condition)
+ * &                  {
+ * ~                       //& Some code to be executed
+ * &                   }
+ * 
+ */
+
+//The while loop loops through a block of code as long as a specified condition is true.
+    // print 1 to 10
+    console.log("print 1 to 10using  (while loop): in js  "); 
+    let i = 1;
+    let text = '';
+
+    while (i <= 10)
+    {
+        text += "\t\n the number is :--> " +i;
+        i++;
+    }
+console.log(text);
+
+console.log("\nExample 2 using  (while loop): in js  "); 
+let myNum = 5;
+while(myNum >= 1)
+{
+    console.log("myNum is :--> " +myNum);
+    myNum++;
+}
